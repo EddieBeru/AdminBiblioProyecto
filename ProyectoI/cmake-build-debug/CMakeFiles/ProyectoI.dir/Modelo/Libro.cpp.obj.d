dcfc3e1a4da4c0f1614d28e5077e82204c27f999
Roy Arias <roy.arias.mejia@est.una.ac.cr>
2025-04-11 16:23:38 -0600

    Clases

---
+++ ProyectoI/cmake-build-debug/CMakeFiles/ProyectoI.dir/Modelo/Libro.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/ProyectoI.dir/Modelo/Libro.cpp.obj: \
+ E:\CLion\AdminBiblioProyecto\ProyectoI\Modelo\Libro.cpp \
+ E:\CLion\AdminBiblioProyecto\ProyectoI\Modelo\Libro.h
---
+++ ProyectoI/cmake-build-debug/CMakeFiles/ProyectoI.dir/Modelo/MaterialDigital.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/ProyectoI.dir/Modelo/MaterialDigital.cpp.obj: \
+ E:\CLion\AdminBiblioProyecto\ProyectoI\Modelo\MaterialDigital.cpp \
+ E:\CLion\AdminBiblioProyecto\ProyectoI\Modelo\MaterialDigital.h
