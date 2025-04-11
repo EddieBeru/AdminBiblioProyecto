7c04adb41b52a2a449051587efc717f43b58151e
Roy Arias <roy.arias.mejia@est.una.ac.cr>
2025-04-11 16:17:58 -0600

    Trabajando en clases

---
+++ ProyectoI/cmake-build-debug/CMakeFiles/ProyectoI.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/CLion/AdminBiblioProyecto/ProyectoI/Modelo/EnLinea.cpp" "CMakeFiles/ProyectoI.dir/Modelo/EnLinea.cpp.obj" "gcc" "CMakeFiles/ProyectoI.dir/Modelo/EnLinea.cpp.obj.d"
+  "E:/CLion/AdminBiblioProyecto/ProyectoI/Modelo/Fisico.cpp" "CMakeFiles/ProyectoI.dir/Modelo/Fisico.cpp.obj" "gcc" "CMakeFiles/ProyectoI.dir/Modelo/Fisico.cpp.obj.d"
+  "E:/CLion/AdminBiblioProyecto/ProyectoI/Modelo/Libro.cpp" "CMakeFiles/ProyectoI.dir/Modelo/Libro.cpp.obj" "gcc" "CMakeFiles/ProyectoI.dir/Modelo/Libro.cpp.obj.d"
+  "E:/CLion/AdminBiblioProyecto/ProyectoI/Modelo/Material.cpp" "CMakeFiles/ProyectoI.dir/Modelo/Material.cpp.obj" "gcc" "CMakeFiles/ProyectoI.dir/Modelo/Material.cpp.obj.d"
+  "E:/CLion/AdminBiblioProyecto/ProyectoI/Modelo/MaterialDigital.cpp" "CMakeFiles/ProyectoI.dir/Modelo/MaterialDigital.cpp.obj" "gcc" "CMakeFiles/ProyectoI.dir/Modelo/MaterialDigital.cpp.obj.d"
+  "E:/CLion/AdminBiblioProyecto/ProyectoI/Modelo/Revista.cpp" "CMakeFiles/ProyectoI.dir/Modelo/Revista.cpp.obj" "gcc" "CMakeFiles/ProyectoI.dir/Modelo/Revista.cpp.obj.d"
+  "E:/CLion/AdminBiblioProyecto/ProyectoI/main.cpp" "CMakeFiles/ProyectoI.dir/main.cpp.obj" "gcc" "CMakeFiles/ProyectoI.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
