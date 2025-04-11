a81dc54a45ec2714c5f7c8bb6471ef3e3e7c6fda
Roy Arias <roy.arias.mejia@est.una.ac.cr>
2025-04-11 16:16:02 -0600

    Material

--- ProyectoI/Modelo/Material.cpp
+++ ProyectoI/Modelo/Material.cpp
@@ -3,3 +3,64 @@
 //
 
 #include "Material.h"
+
+//Definicion del constructor con valores por defecto
+Material::Material(int numClasificacion = 0, int numCatalogo = 0, std::string autores = "", std::string palabrasClave = "",
+         std::string tipoMaterial = "", std::string estadoMaterial = "") {
+    this->numClasificacion = numClasificacion;
+    this->numCatalogo = numCatalogo;
+    this->autores = autores;
+    this->palabrasClave = palabrasClave;
+    this->tipoMaterial = tipoMaterial;
+    this->estadoMaterial = estadoMaterial;
+}
+
+//Setters y Getters
+int Material::get_num_clasificacion() const {
+    return numClasificacion;
+}
+
+void Material::set_num_clasificacion(const int num_clasificacion) {
+    numClasificacion = num_clasificacion;
+}
+
+int Material::get_num_catalogo() const {
+    return numCatalogo;
+}
+
+void Material::set_num_catalogo(const int num_catalogo) {
+    numCatalogo = num_catalogo;
+}
+
+std::string Material::get_autores() const {
+    return autores;
+}
+
+void Material::set_autores(const std::string &autores) {
+    this->autores = autores;
+}
+
+std::string Material::get_palabras_clave() const {
+    return palabrasClave;
+}
+
+void Material::set_palabras_clave(const std::string &palabras_clave) {
+    palabrasClave = palabras_clave;
+}
+
+std::string Material::get_tipo_material() const {
+    return tipoMaterial;
+}
+
+void Material::set_tipo_material(const std::string &tipo_material) {
+    tipoMaterial = tipo_material;
+}
+
+std::string Material::get_estado_material() const {
+    return estadoMaterial;
+}
+
+void Material::set_estado_material(const std::string &estado_material) {
+    estadoMaterial = estado_material;
+}
+
--- ProyectoI/Modelo/Material.h
+++ ProyectoI/Modelo/Material.h
@@ -5,10 +5,58 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
+#include <iostream>
+#include <ostream>
+#include <string>
 
 
 class Material {
+protected:
+    int numClasificacion;
+    int numCatalogo;
+    std::string autores;
+    std::string palabrasClave;
+    std::string tipoMaterial;
+    std::string estadoMaterial;
+public:
+    // Constructor
+    Material(int numClasificacion , int numCatalogo , std::string autores , std::string palabrasClave,
+             std::string tipoMaterial , std::string estadoMaterial);
+    // Destructor
+    virtual ~Material() = default;
 
+    //setters y getters
+    int get_num_clasificacion() const;
+    void set_num_clasificacion(const int num_clasificacion);
+
+    int get_num_catalogo() const;
+    void set_num_catalogo(const int num_catalogo);
+
+    std::string get_autores() const;
+    void set_autores(const std::string &autores);
+
+    std::string get_palabras_clave() const;
+    void set_palabras_clave(const std::string &palabras_clave);
+
+    std::string get_tipo_material() const;
+    void set_tipo_material(const std::string &tipo_material);
+
+    std::string get_estado_material() const;
+    void set_estado_material(const std::string &estado_material);
+
+    //Metodos virtuales
+    virtual std::string getTipo() const = 0; // Método virtual puro para obtener el tipo de material
+    virtual int getDuracionPrestamo() const = 0; // Método virtual puro para obtener la duración del préstamo, se va a trabajar en dias.
+
+    friend std::ostream & operator<<(std::ostream &os, const Material &obj) {
+        return os
+               << "numClasificacion: " << obj.numClasificacion
+               << " numCatalogo: " << obj.numCatalogo
+               << " autores: " << obj.autores
+               << " palabrasClave: " << obj.palabrasClave
+               << " tipoMaterial: " << obj.tipoMaterial
+               << " estadoMaterial: " << obj.estadoMaterial;
+    }
 };
 
 
