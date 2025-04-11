1ab484015d1014edef992ea5d0b825cda463a367
Roy Arias <roy.arias.mejia@est.una.ac.cr>
2025-04-11 13:47:32 -0600

    Creacion de la clase material y modificacion del archivo CMake

---
+++ ProyectoI/Material.h
@@ -0,0 +1,25 @@
+//
+// Created by arias on 4/11/2025.
+//
+
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include <iostream>
+
+class Material {
+private:
+    int numClasificacion;
+    int numCatalogo;
+    std::string autores;
+    std::string palabrasClave;
+    std::string tipoMaterial;
+    std::string estadoMaterial;
+
+public:
+
+};
+
+
+
+#endif //MATERIAL_H
