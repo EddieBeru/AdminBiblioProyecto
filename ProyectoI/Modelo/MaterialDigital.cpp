d3e448a81277345baa2ad14afd05bb4506efa1ee
Felipe Antonio Ugalde Vallejos <148590423+FelipeUgaldeVallejos@users.noreply.github.com>
2025-04-12 22:52:31 -0600

    Update MaterialDigital.cpp

--- ProyectoI/Modelo/MaterialDigital.cpp
+++ ProyectoI/Modelo/MaterialDigital.cpp
@@ -1,5 +1,26 @@
 //
-// Created by arias on 4/11/2025.
+// Created by ugald on 11/4/2025.
 //
 
 #include "MaterialDigital.h"
+
+string MaterialDigital::get_tipo_formato() const {
+    return tipoFormato;
+}
+
+void MaterialDigital::set_tipo_formato(const string &tipo_formato) {
+    tipoFormato = tipo_formato;
+}
+
+int MaterialDigital::getDuracionPrestamo() const {
+    return 5; //se prestan a lo mucho 5 dias
+}
+
+string MaterialDigital::getTipo() const {
+    return "Material Digital: ";
+}
+
+string MaterialDigital::imprimir() const {
+    return "Material Digital: " + std::to_string(get_num_clasificacion()) + ", " + std::to_string(get_num_catalogo()) + ", " +
+           autores + ", " + palabrasClave + ", " + tipoMaterial + ", " + estadoMaterial + ", " + tipoFormato;
+}
