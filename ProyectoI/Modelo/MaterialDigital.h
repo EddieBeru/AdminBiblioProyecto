2a56b9f70df68af57fa1d1be9590ff32d38a6f52
Felipe Antonio Ugalde Vallejos <148590423+FelipeUgaldeVallejos@users.noreply.github.com>
2025-04-12 22:52:13 -0600

    Update MaterialDigital.h

--- ProyectoI/Modelo/MaterialDigital.h
+++ ProyectoI/Modelo/MaterialDigital.h
@@ -1,14 +1,31 @@
 //
-// Created by arias on 4/11/2025.
+// Created by ugald on 11/4/2025.
 //
 
 #ifndef MATERIALDIGITAL_H
 #define MATERIALDIGITAL_H
 
+#include "Material.h"
 
+class MaterialDigital : public Material {
+private:
+    string tipoFormato;
+    public:
+    MaterialDigital(int numClasificacion, int numCatalogo, const std::string &autores, const std::string &palabrasClave,
+        const std::string &tipoMaterial, const std::string &estadoMaterial, const string &tipo_formato)
+        : Material(numClasificacion, numCatalogo, autores, palabrasClave, tipoMaterial, estadoMaterial),
+          tipoFormato(tipo_formato) {
+    }
 
-class MaterialDigital {
+    string get_tipo_formato() const;
 
+    void set_tipo_formato(const string &tipo_formato);
+
+    //metodos virtuales heredados de la clase padre
+    virtual int getDuracionPrestamo() const override;
+    virtual string getTipo() const override;
+
+    virtual string imprimir() const override;
 };
 
 
