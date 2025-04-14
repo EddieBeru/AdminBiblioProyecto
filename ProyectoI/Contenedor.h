8a1610c3b5db0b629841cd5d7163dd810295b8c9
Roy Arias <roy.arias.mejia@est.una.ac.cr>
2025-04-13 20:20:42 -0600

    Edicion de nodo y contenedor

--- ProyectoI/Contenedor.h
+++ ProyectoI/Contenedor.h
@@ -1,8 +1,165 @@
-//
-// Created by arias on 4/11/2025.
-//
-
 #ifndef CONTENEDOR_H
 #define CONTENEDOR_H
+#include <iostream>
+#include "Nodo.h"
+#include <sstream>
+using namespace std;
 
-#endif //CONTENEDOR_H
+template<class T>
+class Contenedor {
+private:
+    Nodo<T>* inicio;
+public:
+    Contenedor(Nodo<T>* = nullptr);
+    ~Contenedor();
+
+    void agregarPrincipio(Nodo<T>*);
+    void agregarFinal(Nodo<T>*);
+    Nodo<T>* getInicio();
+    int tamano();
+	Nodo<T>* buscar(int);
+    bool existe(Nodo<T>*);
+    void eliminar(int);
+    void eliminar(Nodo<T>*);
+    void ordenarAscendente();
+
+	template <class T>
+    friend std::ostream& operator<<(std::ostream& os, const Contenedor<T>& obj);
+
+    string toString();
+};
+
+
+template<class T>
+Contenedor<T>::Contenedor(Nodo<T>* i) {
+    inicio = i;
+}
+
+template<class T>
+Contenedor<T>::~Contenedor() {
+    Nodo<T>* aux = inicio;
+    while (aux != nullptr) {
+        Nodo<T>* aBorrar = aux;
+        aux = aux->sig;
+        delete aBorrar;
+    }
+    delete inicio;
+}
+
+template<class T>
+void Contenedor<T>::agregarPrincipio(Nodo<T>* n) {
+    if (inicio != nullptr) {
+        n->setSig(inicio);
+    }
+    inicio = n;
+}
+
+template<class T>
+void Contenedor<T>::agregarFinal(Nodo<T>* n) {
+    Nodo<T>* aux = inicio;
+    if (aux != nullptr) {
+        while (aux->getSig() != nullptr) {
+
+            aux = aux->getSig();
+        }
+        aux->setSig(n);
+    }
+}
+
+template<class T>
+Nodo<T>* Contenedor<T>::getInicio() {
+    return inicio;
+}
+
+template <class T>
+Nodo<T>* Contenedor<T>::buscar(int i) {
+	Nodo<T>* aux = inicio;
+	int index = 0;
+	while (aux != nullptr && index < i) {
+		aux = aux->getSig();
+		index++;
+	}
+	return aux;
+}
+
+template<class T>
+int Contenedor<T>::tamano() {
+	int count = 0;
+	Nodo<T>* aux = inicio;
+	while (aux != nullptr) {
+		count++;
+		aux = aux->getSig();
+	}
+	return count;
+}
+
+template<class T>
+bool Contenedor<T>::existe(Nodo<T>* n) {
+    Nodo<T>* aux = inicio;
+}
+
+template<class T>
+void Contenedor<T>::eliminar(int i) {
+    int index = 0;
+    Nodo<T>* aux = inicio;
+    while (aux != nullptr && index < i - 1) {
+        aux = aux->getSig();
+        index++;
+    }
+    if (aux != nullptr && aux->getSig() != nullptr) {
+        Nodo<T>* aBorrar = aux->getSig();
+        aux->setSig(aux->getSig()->getSig());
+        delete aBorrar;
+    }
+}
+
+template<class T>
+void Contenedor<T>::eliminar(Nodo<T>* n) {
+    Nodo<T>* aux = inicio;
+    while (aux != nullptr && n != aux->getSig()) {
+        aux = aux->getSig();
+    }
+    if (aux != nullptr && aux->getSig() != nullptr) {
+        Nodo<T>* aBorrar = aux->getSig();
+        aux->setSig(aux->getSig()->getSig());
+        delete aBorrar;
+    }
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const Contenedor<T>& obj) {
+	Nodo<T>* aux = obj.inicio;
+	while (aux != nullptr) {
+		os << aux->getItem() << std::endl;
+		aux = aux->getSig();
+	}
+	return os;
+}
+
+template<class T>
+void Contenedor<T>::ordenarAscendente() {
+    int n = tamano();
+    for (int i = n-1; i >= 0; i--) { // Número de pasadas
+        for (int j = 0; j <= i - 1 ; j++) { // Comparaciones en cada pasada
+            if (buscar(j) < buscar(j + 1)) { // Si están en mal orden, los cambio
+				Nodo<T>* aux = buscar(j);
+				Nodo<T>* aux2 = buscar(j + 1);
+
+                if (aux == inicio) {
+                    Nodo<T>* aux3 = aux2->getSig();
+					inicio = aux2;
+                    aux2->setSig(aux);
+                    aux -> setSig(aux3);
+				} else {
+                    Nodo<T>* aux0 = buscar(j - 1);
+                    Nodo<T>* aux3 = aux2->getSig();
+                    aux0->setSig(aux2);
+                    aux->setSig(aux3);
+					aux2->setSig(aux);
+                }
+
+            }
+        }
+    }
+}
+#endif
--- ProyectoI/main.cpp
+++ ProyectoI/main.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
 
 #include "Modelo/Libro.h"
+#include "Nodo.h"
+#include "Contenedor.h"
 
 int main() {
-
-    // Create an instance of the Libro class
-    Libro libro(12345, 67890, "Autor Ejemplo", "Palabra1, Palabra2", "Tipo Ejemplo", "Estado Ejemplo", "Ubicacion Ejemplo");
-
+	/*
+ Libro libro(12345, 67890, "ejemplo titulo","Autor Ejemplo", "Palabra1, Palabra2", "Tipo Ejemplo", "Estado Ejemplo", "Ubicacion Ejemplo");
+    Revista revista(12345, 67890, "ejemplo titulo","autor ejemplo", "Palabra1, Palabra2", "Tipo Ejemplo", "Estado Ejemplo", "ubicacion ejenplo", 1, 1);
+    MaterialDigital mdigital(12345, 67890,"ejemplo titulo", "Autor Ejemplo", "Palabra1, Palabra2", "Tipo Ejemplo", "Estado Ejemplo", "ejemplo formato");
+    Fisico fisico(12345, 67890,"ejemplo titulo", "Autor Ejemplo", "Palabra1, Palabra2", "Tipo Ejemplo", "Estado Ejemplo", "ejemplo formato", "estilo ejemplo");
+    EnLinea enlinea(12345, 67890,"ejemplo titulo", "Autor Ejemplo", "Palabra1, Palabra2", "Tipo Ejemplo", "Estado Ejemplo", "ejemplo formato", true);
     std::cout << libro << std::endl;
+    std::cout << revista << std::endl;
+    std::cout << mdigital << std::endl;
+    std::cout << fisico << std::endl;
+    std::cout << enlinea << std::endl;
+    std::cout << enlinea.getDuracionPrestamo() << std::endl;
+    */
+    Libro* p = new Libro(111, 1, "Don Quijote", "", "", "", "", "");
+    std::cout << *p << std::endl;
+    Libro* p2 = new Libro(21, 1, "El Principito", "", "", "", "", "");
+    std::cout << *p2 << std::endl;
+    Libro* p3 = new Libro(21, 1, "Diario de Ana Frank", "", "", "", "", "");
+    std::cout << *p2 << std::endl;
+
+    std::cout << ((p < p2) ? "p es menor" : "p2 es mayor o igual") << std::endl;
+    std::cout << ((p <= p2) ? "p es menor o igual" : "p2 es mayor") << std::endl;
+    std::cout << ((p > p2) ? "p es mayor" : "p2 es menor o igual") << std::endl;
+    std::cout << ((p >= p2) ? "p es mayor o igual" : "p2 es menor") << std::endl;
+    std::cout << ((p == p2) ? "p es igual a p2" : "p2 no es igual a p") << std::endl;
+    std::cout << ((p != p2) ? "p no es igual a p2" : "p2 es igual a p") << std::endl;
+
+    Contenedor<Libro>* contenedor = new Contenedor<Libro>();
+	contenedor->agregarPrincipio(new Nodo<Libro>(*p2));
+    contenedor->agregarPrincipio(new Nodo<Libro>(*p));
+	contenedor->agregarFinal(new Nodo<Libro>(*p3));
+    std::cout << *contenedor << std::endl;
+	system("pause");
+
+	contenedor->ordenarAscendente();
+	std::cout << *contenedor << std::endl;
+
     return 0;
 }
-
