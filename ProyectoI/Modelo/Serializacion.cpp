c2112a27b93c61fea75d75f4c2be13e80a205682
Roy Arias <roy.arias.mejia@est.una.ac.cr>
2025-04-23 14:14:46 -0600

    Continuo trabajando en archivos y ahora en serializacion

--- ProyectoI/Modelo/Serializacion.cpp
+++ ProyectoI/Modelo/Serializacion.cpp
@@ -3,3 +3,106 @@
 //
 
 #include "Serializacion.h"
+
+enum TipoMaterial {
+    TIPO_LIBRO,
+    TIPO_REVISTA,
+    TIPO_DIGITAL_ENLINEA,
+    TIPO_DIGITAL_FISICO,
+};
+
+
+void Serializacion::escribir_String(std::ofstream& archivo, const std::string& str) {
+    size_t longS = str.size();
+    archivo.write(reinterpret_cast<const char*>(&longS), sizeof(longS));
+    archivo.write(str.c_str(), longS);
+}
+
+std::string Serializacion::leer_String(std::ifstream &archivo) {
+    size_t longS;
+    archivo.read(reinterpret_cast<char*>(&longS), sizeof(longS));
+    std::string str(longS, '\0');
+    archivo.read(&str[0], longS);
+    return str;
+}
+
+void Serializacion::ser_Material(std::ofstream &archivo, const Material *material) {
+    TipoMaterial tipo;
+
+    if (dynamic_cast<const Libro*>(material)) {
+        tipo = TIPO_LIBRO;
+    } else if (dynamic_cast<const Revista*>(material)) {
+        tipo = TIPO_REVISTA;
+    } else if (dynamic_cast<const EnLinea*>(material)) {
+        tipo = TIPO_DIGITAL_ENLINEA;
+    } else if (dynamic_cast<const Fisico*>(material)) {
+        tipo = TIPO_DIGITAL_FISICO;
+    }
+
+    archivo.write(reinterpret_cast<const char*>(&tipo), sizeof(tipo));
+    int num_Clasificacion = material->get_num_clasificacion();
+    int num_Catalogo = material->get_num_catalogo();
+    archivo.write(reinterpret_cast<const char*>(&num_Clasificacion), sizeof(num_Clasificacion));
+    archivo.write(reinterpret_cast<const char*>(&num_Catalogo), sizeof(num_Catalogo));
+    escribir_String(archivo, material->get_titulo());
+    escribir_String(archivo, material->get_autores());
+    escribir_String(archivo, material->get_palabras_clave());
+    escribir_String(archivo, material->get_tipo_material());
+    escribir_String(archivo, material->get_estado_material());
+}
+
+void Serializacion::des_Material(std::ifstream &archivo, DatosMaterial &datos){
+    archivo.read(reinterpret_cast<char*>(&datos.numClasificacion), sizeof(datos.numClasificacion));
+    archivo.read(reinterpret_cast<char*>(&datos.numCatalogo), sizeof(datos.numCatalogo));
+    datos.titulo = leer_String(archivo);
+    datos.autores = leer_String(archivo);
+    datos.palabrasClave = leer_String(archivo);
+    datos.tipoMaterial = leer_String(archivo);
+    datos.estadoMaterial = leer_String(archivo);
+}
+
+void Serializacion::ser_Libro(std::ofstream &archivo, const Libro *libro) {
+    ser_Material(archivo,libro);
+    escribir_String(archivo, libro->get_ubicacion());
+}
+
+Material *Serializacion::des_Libro(std::ifstream &archivo) {
+    DatosMaterial datos;
+    des_Material(archivo, datos);
+    std::string ubicacion = leer_String(archivo);
+    return new Libro(datos.numClasificacion, datos.numCatalogo, datos.titulo, datos.autores, datos.palabrasClave, datos.tipoMaterial, datos.estadoMaterial, ubicacion);
+}
+
+void Serializacion::ser_Revista(std::ofstream &archivo, const Revista *revista) {
+
+}
+
+Material *Serializacion::des_Revista(std::ifstream &archivo) {
+
+}
+
+void Serializacion::ser_MaterialDigital(std::ofstream &archivo, const MaterialDigital *material) {
+
+}
+
+Material *Serializacion::des_MaterialDigital(std::ifstream &archivo) {
+
+}
+
+void Serializacion::ser_EnLinea(std::ofstream &archivo, const EnLinea *enlinea) {
+
+}
+
+Material *Serializacion::des_EnLinea(std::ifstream &archivo) {
+
+}
+
+void Serializacion::ser_Fisico(std::ofstream &archivo, const Fisico *fisico) {
+
+}
+
+Material *Serializacion::des_Fisico(std::ifstream &archivo) {
+
+}
+
+
--- ProyectoI/Modelo/Serializacion.h
+++ ProyectoI/Modelo/Serializacion.h
@@ -5,10 +5,52 @@
 #ifndef SERIALIZACION_H
 #define SERIALIZACION_H
 
+#include <iostream>
+#include <fstream>
+#include "Material.h"
+#include "Libro.h"
+#include "Revista.h"
+#include "MaterialDigital.h"
+#include "EnLinea.h"
+#include "Fisico.h"
+
+
+struct DatosMaterial {
+    int numClasificacion;
+    int numCatalogo;
+    std::string titulo;
+    std::string autores;
+    std::string palabrasClave;
+    std::string tipoMaterial;
+    std::string estadoMaterial;
+};
 
 
 class Serializacion {
+private:
+    //Escritura y lectura de string
+    static void escribir_String(std::ofstream& archivo, const std::string& str);
+    static std::string leer_String(std::ifstream& archivo);
 
+    static void ser_MaterialDigital(std::ofstream& archivo, const MaterialDigital* material);
+    static Material* des_MaterialDigital(std::ifstream& archivo);
+
+public:
+
+    static void ser_Material(std::ofstream& archivo, const Material* material);
+    static void des_Material(std::ifstream& archivo, DatosMaterial &datos);
+
+    static void ser_Libro(std::ofstream& archivo, const Libro* libro);
+    static Material* des_Libro(std::ifstream& archivo);
+
+    static void ser_Revista(std::ofstream& archivo, const Revista* revista);
+    static Material* des_Revista(std::ifstream& archivo);
+
+    static void ser_EnLinea(std::ofstream& archivo, const EnLinea* enlinea);
+    static Material* des_EnLinea(std::ifstream& archivo);
+
+    static void ser_Fisico(std::ofstream& archivo, const Fisico* fisico);
+    static Material* des_Fisico(std::ifstream& archivo);
 };
 
 
