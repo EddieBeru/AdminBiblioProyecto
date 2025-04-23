72287b6c6345aed7e8f2efe2c9e438a71d4bc3d4
EddieBeru <eddieberu@outlook.com>
2025-04-22 18:20:31 -0600

    Agregué los creditos a Admin Archivos xd

--- ProyectoI/Controlador/AdminArchivos.h
+++ ProyectoI/Controlador/AdminArchivos.h
@@ -1,7 +1,14 @@
-//
-// Created by arias on 4/21/2025.
-//
-
+/*
+* (c) 2025
+* EIF204 - Programación 2
+* 1er Ciclo - 2025
+* NRC 41372 - Grupo 8
+* Proyecto 1
+* 
+* 6-0499-0591; Bermúdez Ureña, Edie
+* 
+* Version 1.0.0 2025-04-22
+*/
 #ifndef ADMINARCHIVOS_H
 #define ADMINARCHIVOS_H
 
